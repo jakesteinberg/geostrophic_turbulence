fixed averaging error in processing of model spectra, first revisit to abaco data

abaco_ship.py
@@ -1,35 +1,36 @@
 # read ABACO shipboard ctd 
 
 import numpy as np
-from scipy.io import netcdf
+from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import glob
 import datetime 
 import gsw
 import pickle
+import time as TT
 from toolkit import unq_searchsorted, plot_pro
 # import scipy.io as si
 
 # --- ctd
-# file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_may_2017/ctd/ab*.cal')
+file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_may_2017/ctd/ab*.cal')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2016/ctd/*.cal')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2015/ctd/*.cal')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_oct_2015/ctd/*.cnv')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_mar_2014/ctd/*.cal')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2013/ctd/*.cal')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2012/ctd/*.cal')
-file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_apr_2011/ctd/*.cal')
+# file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_apr_2011/ctd/*.cal')
 # file_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_mar_2010/ctd/*.cal')
 # --- ladcp
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2018/ladcp/AB*.vel')
-# adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_may_2017/ladcp/AB*.vel')
+adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_may_2017/ladcp/AB*.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2016/ladcp/AB*d.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2015/ladcp/AB*d.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_oct_2015/ladcp/AB*d.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_mar_2014/ladcp/AB*d.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2013/ladcp/AB*d.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_feb_2012/ladcp/AB*d.vel')
-adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_apr_2011/ladcp/AB*d.vel')
+# adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_apr_2011/ladcp/AB*d.vel')
 # adcp_list = glob.glob('/Users/jake/Desktop/abaco/abaco_ship_mar_2010/ladcp/AB*d.vel')  # not lowered deep at all
 
 # ---- seemingly all except 2017
@@ -62,7 +63,7 @@ Lat = np.nan * np.ones(len(file_list))
 for m in range(len(file_list)):
     this_file = file_list[m]
     count_r = 0
-    f = open(this_file, encoding="ISO-8859-1")
+    f = open(this_file)  # , encoding="ISO-8859-1")
     initial = f.readlines()
     # extract cast lat/lon
     line_2 = initial[1].strip().split("\t")
@@ -104,6 +105,8 @@ O1 = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
 O2 = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
 SA = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
 CT = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
+T = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
+S = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
 sig0 = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
 # lat = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
 # lon = np.nan*np.ones((np.size(bin_press), np.size(file_list)))
@@ -113,6 +116,7 @@ for i in range(np.size(file_list)):
     #                                    usecols=(np.arange(0, 16)), unpack='True'))
     load_dat = Data[i]
     dep = -1 * gsw.z_from_p(load_dat[:, 0], Lat[i] * np.ones(len(load_dat[:, 0])))
+    print(np.nanmax(dep))
     # dep = -1 * gsw.z_from_p(load_dat[:, 3], load_dat[:, 10])  # all others?
     # dep = -1 * gsw.z_from_p(load_dat[:, 2], load_dat[:, 9])     # oct 2015
     for j in range(1, np.size(bin_depth)-1):
@@ -135,9 +139,9 @@ for i in range(np.size(file_list)):
             this_T[T_bad[0]] = np.nan
             # this_T[T_bad[0], T_bad[1]] = np.nan
 
-        this_O = load_dat[p_in, 6]
+        # this_O = load_dat[p_in, 6]
         # this_O = load_dat[p_in, 23:25]  # --- all others?
-        # this_O = load_dat[p_in, 14:16]    # --- may 2017 (in units of ml/l)
+        this_O = load_dat[p_in, 14:16]    # --- may 2017 (in units of ml/l)
         # this_O = load_dat[p_in, 3:5]    # --- oct 2015
         O_bad = np.where(this_O < 0)
         if np.size(O_bad) > 0:
@@ -168,6 +172,8 @@ for i in range(np.size(file_list)):
 
     SA[:, i] = gsw.SA_from_SP(S1[:, i], bin_press, Lon[i] * np.ones(len(S1[:, i])), Lat[i] * np.ones(len(S1[:, i])))
     CT[:, i] = gsw.CT_from_t(S1[:, i], T1[:, i], bin_press)
+    T[:, i] = T1[:, i]
+    S[:, i] = S1[:, i]
     sig0[:, i] = gsw.sigma0(SA[:, i], CT[:, i])
 
 # -------------------------------------------------------------------------------------------------------
@@ -213,16 +219,20 @@ Lat_1 = Lat[dist_dive > 5]
 Lon_1 = Lon[dist_dive > 5]
 SA = SA[:, dist_dive > 5]
 CT = CT[:, dist_dive > 5]
+T = T[:, dist_dive > 5]
+S = S[:, dist_dive > 5]
 O2 = O1[:, dist_dive > 5]
 dist_dive_2 = dist_dive[dist_dive > 5]
 number_profiles = len(dist_dive_2)
 
 ordered = np.argsort(dist_dive_2)
 sig0 = sig0[:, ordered]
-Lat_2 = Lat[ordered]
-Lon_2 = Lon[ordered]
+Lat_2 = Lat_1[ordered]
+Lon_2 = Lon_1[ordered]
 SA = SA[:, ordered]
 CT = CT[:, ordered]
+T = T[:, ordered]
+S = S[:, ordered]
 O2 = O1[:, ordered]
 dist_dive_3 = dist_dive_2[ordered]
 
@@ -243,8 +253,8 @@ lat_uv = np.nan*np.ones(np.size(adcp_list))
 lon_uv = np.nan*np.ones(np.size(adcp_list))
 time_uv = np.nan*np.ones(np.size(adcp_list))
 for i in range(np.size(adcp_list)):
-    # test3 = open(adcp_list[i], 'r')  # for 2017
-    test3 = open(adcp_list[i], 'r', encoding="ISO-8859-1")  # for all others
+    test3 = open(adcp_list[i], 'r')  # for 2017
+    # test3 = open(adcp_list[i], 'r', encoding="ISO-8859-1")  # for all others
     line1 = test3.readline()
     line2 = test3.readline()
     line3 = test3.readline()
@@ -256,10 +266,15 @@ for i in range(np.size(adcp_list)):
     a, b = unq_searchsorted(load_dat[:, 0], dac_bin_dep)
     u[np.where(b)[0], i] = load_dat[:, 1]
     v[np.where(b)[0], i] = load_dat[:, 2]
-    lat_uv[i] = int(line2[4:6]) + np.float64(line2[7:14])/60  # 7:14, except 2017 = 8:14
-    lon_uv[i] = -1*(int(line3[4:6]) + np.float64(line3[7:14])/60)  # 7:14, except 2017 = 8:14
+    # lat_uv[i] = int(line2[4:6]) + np.float64(line2[7:14])/60  # 7:14, except 2017 = 8:14
+    # lon_uv[i] = -1*(int(line3[4:6]) + np.float64(line3[7:14])/60)  # 7:14, except 2017 = 8:14
+    # # 24:33 for 2013,  11:19 for 2014,  7:16 for 2011 and 2012 and 2017, 42:51 for 2018, 16:25 for 2010, 8:16 for others
+    # time_uv[i] = np.float64(line4[5:16])
+    # special for running older python (to run matlab)
+    lat_uv[i] = int(line2[4:6]) + np.float64(line2[8:14])/60  # 7:14, except 2017 = 8:14
+    lon_uv[i] = -1*(int(line3[4:6]) + np.float64(line3[8:14])/60)  # 7:14, except 2017 = 8:14
     # 24:33 for 2013,  11:19 for 2014,  7:16 for 2011 and 2012 and 2017, 42:51 for 2018, 16:25 for 2010, 8:16 for others
-    time_uv[i] = np.float64(line4[7:16])
+    time_uv[i] = np.float64(line4[35:46])
       
 # compute cross-shore distance and plot (non)-geostrophic velocity
 
@@ -295,6 +310,24 @@ matlab_datenum = 731965.04835648148
 t_s = datetime.date.fromordinal(int(np.min(time_uv))) + datetime.timedelta(days=matlab_datenum%1) - datetime.timedelta(days=366)
 t_e = datetime.date.fromordinal(int(np.max(time_uv))) + datetime.timedelta(days=matlab_datenum%1) - datetime.timedelta(days=366)
 
+# # --- convert in matlab
+import matlab.engine
+eng = matlab.engine.start_matlab()
+eng.addpath(r'/Users/jake/Documents/MATLAB/eos80_legacy_gamma_n/')
+eng.addpath(r'/Users/jake/Documents/MATLAB/eos80_legacy_gamma_n/library/')
+gamma = np.nan * np.ones(np.shape(CT))
+print('Opened Matlab')
+
+tic = TT.clock()
+for i in range(len(dist_dive_3)):
+    good_ind = np.where(~np.isnan(S[:, i]))[0]
+    gamma[good_ind, i] = np.squeeze(np.array(eng.eos80_legacy_gamma_n(
+        matlab.double(S[good_ind, i].tolist()), matlab.double(T[good_ind, i].tolist()),
+        matlab.double(bin_press[good_ind].tolist()), matlab.double([Lon_2[i]]), matlab.double([Lat_2[0]]))))
+toc = TT.clock()
+# print('Time step = ' + str(j) + ' = '+ str(toc - tic) + 's')
+eng.quit()
+print('Closed Matlab')
 
 fig, (ax1, ax2) = plt.subplots(1, 2)
 ax1.scatter(Lon, Lat, s=2, color='r')
@@ -302,12 +335,12 @@ ax1.scatter(lon_uv, lat_uv, s=2, color='k')
 ax1.grid()
 lv = np.arange(-60, 60, 5)
 lv2 = np.arange(-60, 60, 10)
-dn_lv = np.concatenate((np.arange(25, 27.8, 0.2), np.arange(27.8, 28, 0.025)))
+dn_lv = np.concatenate((np.arange(25, 27.8, 0.2), np.arange(27.8, 28.5, 0.025)))
 order = np.argsort(adcp_dist)
 ad = ax2.contourf(adcp_dist[order], dac_bin_dep, V[:, order], levels=lv)
 ax2.contour(adcp_dist[order], dac_bin_dep, V[:, order], levels=lv2, colors='k')
-den_c = ax2.contour(np.tile(dist_dive_3[None, :], (len(bin_depth), 1)), np.tile(bin_depth[:, None], (1, number_profiles)), sig0,
-                    levels=dn_lv, colors='r', linewidth=.75)
+den_c = ax2.contour(np.tile(dist_dive_3[None, :], (len(bin_depth), 1)), np.tile(bin_depth[:, None], (1, number_profiles)), gamma,
+                    levels=dn_lv, colors='r', linewidths=.5)
 ax2.clabel(den_c, fontsize=6, inline=1, fmt='%.4g', spacing=10)
 ax2.set_title('ABACO SHIP: ' + np.str(t_s.month) + '/' +
               np.str(t_s.day) + '/' + np.str(t_s.year) + ' - ' + np.str(t_e.month) +
@@ -326,19 +359,57 @@ plot_pro(ax2)
 # write python dict to a file
 sa = 1
 if sa > 0:
+
+    dataset = Dataset('/Users/jake/Documents/baroclinic_modes/Shipboard/ABACO/ship_ladcp_2017.nc', 'w', format='NETCDF4_CLASSIC')
+    dep_dim = dataset.createDimension('dep_dim', len(bin_depth))
+    dist_dim = dataset.createDimension('dist_dim', len(dist_dive_3))
+    adcp_dim = dataset.createDimension('adcp_dim', len(time_uv))
+    adcp_dep_dim = dataset.createDimension('adcp_dep_dim', len(dac_bin_dep))
+    bin_dep_out = dataset.createVariable('depth', np.float64, ('dep_dim'))
+    bin_dep_out[:] = bin_depth
+    dist_out = dataset.createVariable('dist', np.float64, ('dist_dim'))
+    dist_out[:] = dist_dive_3
+    gamma_out = dataset.createVariable('gamma', np.float64, ('dep_dim', 'dist_dim'))
+    gamma_out[:] = gamma
+    CT_out = dataset.createVariable('CT', np.float64, ('dep_dim', 'dist_dim'))
+    CT_out[:] = CT
+    SA_out = dataset.createVariable('SA', np.float64, ('dep_dim', 'dist_dim'))
+    SA_out[:] = SA
+    Lon_out = dataset.createVariable('Cast_Lon', np.float64, ('dist_dim'))
+    Lon_out[:] = Lon_2
+    Lat_out = dataset.createVariable('Cast_Lat', np.float64, ('dist_dim'))
+    Lat_out[:] = Lat_2
+
+    time_uv_out = dataset.createVariable('time_uv', np.float64, ('adcp_dim'))
+    time_uv_out[:] = time_uv
+    lon_uv_out = dataset.createVariable('Lon_uv', np.float64, ('adcp_dim'))
+    lon_uv_out[:] = lon_uv
+    lat_uv_out = dataset.createVariable('Lat_uv', np.float64, ('adcp_dim'))
+    lat_uv_out[:] = lat_uv
+    dep_uv_out = dataset.createVariable('dep_uv', np.float64, ('adcp_dep_dim'))
+    dep_uv_out[:] = dac_bin_dep
+    dist_uv_out = dataset.createVariable('dist_uv', np.float64, ('adcp_dim'))
+    dist_uv_out[:] = adcp_dist
+    u_out = dataset.createVariable('adcp_u', np.float64, ('adcp_dep_dim', 'adcp_dim'))
+    u_out[:] = U
+    v_out = dataset.createVariable('adcp_v', np.float64, ('adcp_dep_dim', 'adcp_dim'))
+    v_out[:] = V
+    dataset.close()
+
     # mydict = {'adcp_depth': dac_bin_dep, 'adcp_u': U, 'adcp_v': V,
     #           'adcp_lon': lon_uv[adcp_in], 'adcp_lat': lat_uv[adcp_in], 'adcp_dist': adcp_dist,
     #           'time_uv': time_uv[adcp_in]}
     # output = open('/Users/jake/Documents/baroclinic_modes/Shipboard/ABACO/ship_ladcp_' + str(t_s) + '.pkl', 'wb')
     # pickle.dump(mydict, output)
     # output.close()
-    mydict = {'bin_depth': bin_depth, 'adcp_depth': dac_bin_dep, 'adcp_u': U, 'adcp_v': V,
-              'adcp_lon': lon_uv, 'adcp_lat': lat_uv, 'adcp_dist': adcp_dist,
-              'den_grid': sig0, 'den_dist': dist_dive_3, 'SA': SA, 'CT': CT, 'cast_lon': Lon_2, 'cast_lat': Lat_2,
-              'time_uv': time_uv, 'oxygen': O2}
-    output = open('/Users/jake/Documents/baroclinic_modes/Shipboard/ABACO/ship_ladcp_' + str(t_s) + '.pkl', 'wb')
-    pickle.dump(mydict, output)
-    output.close()
+
+    # mydict = {'bin_depth': bin_depth, 'adcp_depth': dac_bin_dep, 'adcp_u': U, 'adcp_v': V,
+    #           'adcp_lon': lon_uv, 'adcp_lat': lat_uv, 'adcp_dist': adcp_dist,
+    #           'den_grid': sig0, 'den_dist': dist_dive_3, 'SA': SA, 'CT': CT, 'cast_lon': Lon_2, 'cast_lat': Lat_2,
+    #           'time_uv': time_uv, 'oxygen': O2}
+    # output = open('/Users/jake/Documents/baroclinic_modes/Shipboard/ABACO/ship_ladcp_' + str(t_s) + '.pkl', 'wb')
+    # pickle.dump(mydict, output)
+    # output.close()
 
 
 # to change

model_testing_of_glider_profiling_3.py
@@ -17,7 +17,7 @@ from zrfun import get_basic_info, get_z
 
 # because we load pickle protocol 2 (needed for matlab engine) we need 'glider' environment (not 'geo_env')
 
-this_path = 'e_w_extraction_nov28_nov30_offshore'  # 'n_s_extraction_eddy_nov1_nov3'  #
+this_path = 'e_w_extraction_nov22_nov24_offshore'  # 'n_s_extraction_eddy_nov1_nov3'  #
 
 # -- LOAD extracted and PROCESSED MODEL TIME STEPS WITH COMPUTED GAMMA
 # this file has combined all model output and computed gamma (using model_testing_of_glider_profiling_2.py)
@@ -76,19 +76,21 @@ rho0 = 1025.0
 # time between each model time step is 1hr.
 
 # main set of parameters to adjust
-dg_vertical_speed = 0.075  # m/s
+dg_vertical_speed = 0.08  # m/s
 dg_glide_slope = 3
 num_dives = 3
 dac_error = 0.01  # m/s
-y_dg_s = 70000     # horizontal position, start of glider dives (75km)
+y_dg_s = 25000     # horizontal position, start of glider dives (75km)
 z_dg_s = 0        # depth, start of glider dives
 partial_mw = 0    # include exclude partial m/w estimates
 
 t_s = datetime.date.fromordinal(np.int(time_ord_s[0]))
 t_e = datetime.date.fromordinal(np.int(time_ord_s[-1]))
 tag = str(t_s.month) + '_' + str(t_s.day) + '_' + str(t_e.month) + '_' + str(t_e.day)
-output_filename = '/Users/jake/Documents/baroclinic_modes/Model/vel_anom_slow2_calm_' + tag + '.pkl'
-save_anom = 1
+output_filename = '/Users/jake/Documents/baroclinic_modes/Model/vel_anom_y70_v08_slp3_' + tag + '.pkl'
+save_anom = 0
+save_p = 0
+save_p_g = 0
 
 # need to specify D_TGT or have glider 'fly' until it hits bottom
 data_loc = np.nanmean(sig0_out_s, axis=2)  # (depth X xy_grid)
@@ -246,38 +248,42 @@ else:
 # model velocity profiles at locations within domain spanned by one m/w pattern and in the same time
 u_model = np.flipud(u_out_s)
 u_mod_at_mw = []
+gamma_mod_at_mw = []
 u_mod_at_mw_avg = np.nan * np.ones((len(dg_z), len(dg_dac_mid)))
+gamma_mod_at_mw_avg = np.nan * np.ones((len(dg_z), len(dg_dac_mid)))
 for i in range(len(dg_dac_mid)):
-    # time bounds of m/w profiles
-    if i <= 1:
+    # time/space bounds of m/w profiles
+    # incomplete M
+    if i < 1:
         min_t = dg_t[0, 0]
-        max_t = mw_time[i + 2]
-        min_y = 0
-        max_y = dg_dac_mid[i + 2]
-    elif (i > 1) & (i < len(dg_dac_mid) - 2):
-        min_t = mw_time[i - 2]
-        max_t = mw_time[i + 2]
-        min_y = dg_dac_mid[i - 2]
-        max_y = dg_dac_mid[i + 2]
-    elif i > len(dg_dac_mid) - 2:
-        min_t = mw_time[i - 2]
-        max_t = dg_t[0, -1]
-        min_y = dg_dac_mid[i - 2]
-        max_y = np.nanmax(dg_y)
+        max_t = np.nanmax(dg_t[:, 2])
+        min_y = np.nanmin(dg_y[:, 0])
+        max_y = np.nanmean(dg_y[:, 2])
+    elif (i > 0) & (i < len(dg_dac_mid) - 2):
+        min_t = np.nanmin(dg_t[:, i - 1])
+        max_t = np.nanmax(dg_t[:, i + 2])
+        min_y = np.nanmin(dg_y[:, i - 1])
+        max_y = np.nanmax(dg_y[:, i + 2])
+    elif i >= len(dg_dac_mid) - 2:
+        min_t = np.nanmin(dg_t[:, i - 1])
+        max_t = np.nanmax(dg_t[:, -1])
+        min_y = np.nanmin(dg_y[:, i - 1])
+        max_y = np.nanmax(dg_y[:, -1])
+
     mtun = np.where(time_ord_s <= min_t)[0][-1]
     mtov = np.where(time_ord_s > max_t)[0][0]
     this_ygs = np.where(xy_grid <= min_y)[0][-1]
     this_yge = np.where(xy_grid >= max_y)[0][0]
 
-    u_mod_at_mw.append(u_model[:, this_ygs+1:this_yge, mtun:mtov])
-    u_mod_at_mw_avg[:, i] = np.nanmean(np.nanmean(u_model[:, this_ygs+1:this_yge, mtun:mtov], axis=2), axis=1)
+    print(str(min_t) + '-' + str(max_t))
+    print(str(time_ord_s[mtun]) + '-' + str(time_ord_s[mtov]))
+    print(str(min_y) + '-' + str(max_y))
+    print(str(xy_grid[this_ygs]) + '-' + str(xy_grid[this_yge]))
 
-    # mean_t = np.nanmean(dg_t[:, i:i + 2])  # mean time of one dive_climb cycle
-    # nearest_t = np.where(time_ord_s < mean_t)[0][-1]
-    # mw_time[i] = np.nanmean(time_ord_s[nearest_t:nearest_t + 2])
-    # avg_u_at = np.nanmean(u_out_s[:, :, nearest_t:nearest_t + 2], axis=2)
-    # for j in range(len(z_grid)):
-    #     u_mod_at_mw_y[j, i] = np.interp(mw_y[i], xy_grid, avg_u_at[j, :])
+    u_mod_at_mw.append(u_model[:, this_ygs+1:this_yge, mtun:mtov])
+    gamma_mod_at_mw.append(data_interp[:, this_ygs+1:this_yge, mtun:mtov])
+    u_mod_at_mw_avg[:, i] = np.nanmean(np.nanmean(u_model[:, this_ygs+1:this_yge, mtun:mtov], axis=2), axis=1)
+    gamma_mod_at_mw_avg[:, i] = np.nanmean(np.nanmean(data_interp[:, this_ygs+1:this_yge, mtun:mtov], axis=2), axis=1)
 
 # ----------------------------------------------------------------------------------------------------------------------
 # transect plan view with DAC
@@ -466,8 +472,8 @@ print('Completed M/W Vel Estimation')
 # --- PLOTTING
 h_max = np.nanmax(dg_y/1000 + 20)  # horizontal domain limit
 z_max = -3150
-u_levels = np.array([-.3, -.25, - .2, -.15, -.125, -.1, -.075, -.05, -0.025, 0,
-                     0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.2, 0.25, 0.3])
+u_levels = np.array([-0.4, -0.3, -.25, - .2, -.15, -.125, -.1, -.075, -.05, -0.025, 0,
+                     0.025, 0.05, 0.075, 0.1, 0.125, 0.15, 0.2, 0.25, 0.3, 0.4])
 plot0 = 1
 if plot0 > 0:
     matplotlib.rcParams['figure.figsize'] = (12, 6)
@@ -519,8 +525,8 @@ if plot0 > 0:
     ax3.set_xlabel('E/W distance [km]')
     ax3.set_xlim([y_dg_s/1000 - 5, np.nanmax(dg_y/1000) + 5])
     plot_pro(ax3)
-    # f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_cross.png",
-    #           dpi=200)
+    if save_p > 0:
+        f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_ind_cross_2.png", dpi=200)
 
 # ------------------------------
 # model - glider velocity error
@@ -962,7 +968,8 @@ ax[3].set_ylim([27.9, 28])
 ax[3].invert_yaxis()
 ax[3].set_xlabel('Transect Distance [km]')
 plot_pro(ax[3])
-# f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_glider_den_grad.png", dpi=200)
+if save_p_g > 0:
+    f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_ind_den_grad_3.png", dpi=200)
 # --------------------------------------------------------------------------------------------------------------------
 # --- Background density
 pkl_file = open('/Users/jake/Documents/baroclinic_modes/Model/background_density.pkl', 'rb')
@@ -1000,7 +1007,7 @@ deep_shr_max = 0.1
 deep_shr_max_dep = 2000.0
 # fit limits
 eta_fit_dep_min = 200.0
-eta_fit_dep_max = 2500.0
+eta_fit_dep_max = 2600.0
 
 # adjust z_grid and N2 profile such that N2=0 at surface and that z_grid min = -2500
 # match background z_grid and N2 to current data
@@ -1021,9 +1028,8 @@ G, Gz, c, epsilon = vertical_modes(avg_N2, -1.0 * z_grid_n2, omega, mmax)  # N2
 #
 print('Computed Vertical Modes from Background Profiles')
 
-
-# DG ETA
-# -- eta from individual profiles
+# ---------------------------------------------------------------------------------------------------------------------
+# -- DG eta from individual profiles
 eta = np.nan * np.ones(np.shape(dg_sig0))
 AG_dg = np.zeros((nmodes, num_profs))
 PE_per_mass_dg = np.nan * np.ones((nmodes, num_profs))
@@ -1061,11 +1067,9 @@ for i in range(num_profs):
     #                                                              np.transpose(np.atleast_2d(eta[good, i])),
     #                                                              eta_fit_dep_min, eta_fit_dep_max)
 
-
 G, Gz, c, epsilon = vertical_modes(avg_N2, -1.0 * z_grid_n2, omega, mmax)  # N2
 # ---------------------------------------------------------------------------------------------------------------------
-
-# -- eta avg across four profiles
+# -- DG eta avg across four profiles
 eta_sm = np.nan * np.ones(np.shape(avg_sig_pd))
 AG_dg_sm = np.zeros((nmodes, num_profs_eta))
 PE_per_mass_dg_sm = np.nan * np.ones((nmodes, num_profs_eta))
@@ -1103,17 +1107,21 @@ for i in range(np.shape(avg_sig_pd)[1]):
         #                                                                  nmodes, avg_N2, G, c, eta_sm[0:126, :],
         #                                                                  eta_fit_dep_min, eta_fit_dep_max)
 
+# ---------------------------------------------------------------------------------------------------------------------
 # -- eta Model
 in_range = np.where((xy_grid > np.nanmin(dg_y)) & (xy_grid < np.nanmax(dg_y)))[0]
 eta_model = np.nan * np.ones(np.shape(sig0_out_s[:, in_range, 0]))
-AG_model = np.zeros((nmodes, len(in_range)))
-PE_per_mass_model = np.nan * np.ones((nmodes, len(in_range)))
-eta_m_model = np.nan * np.ones((len(dg_z), len(in_range)))
-Neta_m_model = np.nan * np.ones((len(dg_z), len(in_range)))
-for i in range(len(in_range)):
+
+eta_model = np.nan * np.ones(np.shape(gamma_mod_at_mw_avg))
+AG_model = np.zeros((nmodes, np.shape(eta_model)[1]))
+PE_per_mass_model = np.nan * np.ones((nmodes, np.shape(eta_model)[1]))
+eta_m_model = np.nan * np.ones((len(dg_z), np.shape(eta_model)[1]))
+Neta_m_model = np.nan * np.ones((len(dg_z), np.shape(eta_model)[1]))
+for i in range(np.shape(eta_model)[1]):
     # range over which m/w profile takes up,
     # consider each model density profile, avg at each grid point in time, estimate eta
-    this_model = np.flipud(np.nanmean(sig0_out_s[:, in_range[i], :], axis=1))
+    # this_model = np.flipud(np.nanmean(sig0_out_s[:, in_range[i], :], axis=1))
+    this_model = gamma_mod_at_mw_avg[:, i]
 
     good = np.where(~np.isnan(this_model))[0]
     avg_sig0_match = np.interp(np.abs(dg_z[good]), np.abs(z_grid_n2), avg_sig0)
@@ -1146,9 +1154,9 @@ for i in range(len(in_range)):
     #                                                              nmodes, avg_N2, G, c, eta_model[0:126, :],
     #                                                              eta_fit_dep_min, eta_fit_dep_max)
 
-
-# --- fit velocities ---
-# DG
+# ---------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+# --- DG velocities ---
 HKE_per_mass_dg = np.nan * np.zeros([nmodes, num_profs_eta])
 modest = np.arange(11, nmodes)
 good_ke_prof = np.ones(num_profs_eta)
@@ -1177,16 +1185,17 @@ for i in range(num_profs_eta):
                 good_ke_prof[i] = 0  # flag profile as noisy
         else:
             good_ke_prof[i] = 0  # flag empty profile as noisy as well
-
+# ---------------------------------------------------------------------------------------------------------------------
 # Model
-HKE_per_mass_model = np.nan * np.zeros([nmodes, np.shape(eta_model)[1]])
-modest = np.arange(11, nmodes)
-good_ke_prof = np.ones(len(in_range))
-AGz_model = np.zeros([nmodes, len(in_range)])
+# select avg model profiles that coincide with dg velocity profiles in space and time
+avg_mod_u = u_mod_at_mw_avg[:, 1:-1]
+HKE_per_mass_model = np.nan * np.zeros([nmodes, np.shape(avg_mod_u)[1]])
+good_ke_prof = np.ones(np.shape(avg_mod_u)[1])
+AGz_model = np.zeros([nmodes, np.shape(avg_mod_u)[1]])
 HKE_noise_threshold = 1e-5  # 1e-5
-V_m_model = np.nan * np.zeros((len(dg_z), len(in_range)))
-avg_mod_u = np.flipud(np.nanmean(u_out_s[:, in_range, :], axis=2))
-for i in range(len(in_range)):
+V_m_model = np.nan * np.zeros((len(dg_z), np.shape(avg_mod_u)[1]))
+# avg_mod_u = np.flipud(np.nanmean(u_out_s[:, in_range, :], axis=2))
+for i in range(np.shape(avg_mod_u)[1]):
     good = np.where(~np.isnan(avg_mod_u[:, i]))[0]
     avg_N2_match = np.interp(np.abs(dg_z[good]), np.abs(z_grid_n2), avg_N2)
 
@@ -1222,28 +1231,32 @@ ax1.set_ylim([z_max, 0])
 ax1.set_ylabel('Z [m]')
 ax1.set_xlabel('Density Anomaly')
 ax1.set_title('Density Anomaly')
-for i in range(np.shape(eta)[1]):
-    ax2.plot(eta[:, i], dg_z, color='#4682B4', linewidth=1.25)
-    ax2.plot(eta_m_dg[:, i], dg_z, color='k', linestyle='--', linewidth=.75)
+for i in range(np.shape(eta_sm)[1]):
+    ax2.plot(eta_sm[:, i], dg_z, color='#4682B4', linewidth=1.25, label='DG')
+    ax2.plot(eta_m_dg_sm[:, i], dg_z, color='k', linestyle='--', linewidth=.75)
+    ax2.plot(eta_model[0:-20, i + 1], dg_z[0:-20], linewidth=0.5, color='r', label='Model')
 ax2.grid()
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend([handles[-2], handles[-1]], [labels[-2], labels[-1]], fontsize=10)
 ax2.set_xlabel('Isopycnal Displacement [m]')
 ax2.set_title('Vertical Displacement')
 
-avg_mod_u = np.flipud(np.nanmean(u_out_s[:, in_range, :], axis=2))
-for i in range(np.shape(avg_mod_u)[1]):
-    ax3.plot(avg_mod_u[:, i], dg_z, linewidth=0.75, color='#DCDCDC')
-    ax3.plot(V_m_model[:, i], dg_z, color='k', linestyle='--', linewidth=.4)
 for i in range(np.shape(v_g)[1]):
-    ax3.plot(v_g[:, i], dg_z)
+    ax3.plot(v_g[:, i], dg_z, color='#4682B4', linewidth=1.25, label='DG')
     ax3.plot(V_m[:, i], dg_z, color='k', linestyle='--', linewidth=.75)
+    ax3.plot(avg_mod_u[0:-20, i], dg_z[0:-20], color='r', linewidth=0.5, label='Model')
+handles, labels = ax2.get_legend_handles_labels()
+ax2.legend([handles[-2], handles[-1]], [labels[-2], labels[-1]], fontsize=10)
 ax2.set_xlim([-200, 200])
 ax3.set_xlim([-.4, .4])
 ax3.set_title('Model and DG Velocity (u)')
 ax3.set_xlabel('Velocity [m/s]')
 plot_pro(ax3)
+if save_p > 0:
+    f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_ind_vel_eta_2.png", dpi=200)
 
 # ---------------------------------------------------------------------------------------------------------------------
-# select only dg profiles with dephts greater than 2000m
+# select only dg profiles with depths greater than 2000m
 max_eta_dep = np.nan * np.ones(np.shape(eta)[1])
 for i in range(np.shape(eta)[1]):
     max_eta_dep[i] = dg_z[np.where(~np.isnan(eta[:, i]))[0][-1]]
@@ -1254,8 +1267,8 @@ for i in range(np.shape(eta_sm)[1]):
     max_eta_sm_dep[i] = dg_z[np.where(~np.isnan(eta_sm[:, i]))[0][-1]]
     max_v_dep[i] = dg_z[np.where(~np.isnan(v_g[:, i]))[0][-1]]
 
-max_model_dep = np.nan * np.ones(np.shape(PE_per_mass_model)[1])
-for i in range(np.shape(PE_per_mass_model)[1]):
+max_model_dep = np.nan * np.ones(np.shape(avg_mod_u)[1])
+for i in range(np.shape(avg_mod_u)[1]):
     max_model_dep[i] = dg_z[np.where(~np.isnan(avg_mod_u[:, i]))[0][-1]]
 
 good_eta = np.where(max_eta_dep < -2000)[0]
@@ -1331,14 +1344,16 @@ handles, labels = ax1.get_legend_handles_labels()
 ax1.legend(handles, labels, fontsize=12)
 handles, labels = ax2.get_legend_handles_labels()
 ax2.legend(handles, labels, fontsize=12)
-
 plot_pro(ax2)
+if save_p > 0:
+    f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_ind_energy_2.png", dpi=200)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------
 # save?
 if save_anom:
     my_dict = {'dg_z': dg_z, 'dg_v': v_g, 'model_u_at_mwv': u_mod_at_mw,
+               'model_u_at_mw_avg': avg_mod_u,
                'shear_error': shear_error, 'igw_var': den_var,
                'KE_dg': HKE_per_mass_dg, 'KE_mod': HKE_per_mass_model,
                'eta_m_dg': eta_m_dg, 'PE_dg': PE_per_mass_dg,

model_testing_of_glider_profiling_4.py
@@ -14,8 +14,9 @@ from toolkit import plot_pro, nanseg_interp
 from zrfun import get_basic_info, get_z
 
 
-file_list = glob.glob('/Users/jake/Documents/baroclinic_modes/Model/vel_anom_slow2*.pkl')
-savee = 0
+file_list = glob.glob('/Users/jake/Documents/baroclinic_modes/Model/vel_anom_y*.pkl')
+tagg = 'yall_v08_slp3'
+savee = 1
 
 direct_anom = []
 # igw_var = np.nan * np.ones(len(file_list))
@@ -26,6 +27,7 @@ for i in range(len(file_list)):
 
     glider_v = MOD['dg_v'][:]
     model_v = MOD['model_u_at_mwv']
+    model_v_avg = MOD['model_u_at_mw_avg']
     z_grid = MOD['dg_z'][:]
     slope_error = MOD['shear_error'][:]
     igw = MOD['igw_var']
@@ -51,6 +53,7 @@ for i in range(len(file_list)):
     if i < 1:
         v = glider_v.copy()
         mod_v = model_mean_per_mw.copy()
+        mod_v_avg = model_v_avg.copy()
         mod_space_out = mod_space
         mod_time_out = mod_time
         anoms = glider_v - model_mean_per_mw
@@ -66,6 +69,7 @@ for i in range(len(file_list)):
     else:
         v = np.concatenate((v, glider_v.copy()), axis=1)
         mod_v = np.concatenate((mod_v, model_mean_per_mw), axis=1)
+        mod_v_avg = np.concatenate((mod_v_avg, model_v_avg), axis=1)
         mod_space_out = np.concatenate((mod_space_out, mod_space), axis=1)
         mod_time_out = np.concatenate((mod_time_out, mod_time), axis=1)
         anoms = np.concatenate((anoms, glider_v - model_mean_per_mw), axis=1)
@@ -84,7 +88,7 @@ matplotlib.rcParams['figure.figsize'] = (6.5, 8)
 f, ax = plt.subplots()
 low_er_mean = np.nan * np.ones(len(z_grid))
 for i in range(len(z_grid)):
-    low = np.where(igw_var[i, :] < .2)[0]
+    low = np.where(igw_var[i, :] < .25)[0]
     ax.scatter(slope_er[i, :], z_grid[i] * np.ones(len(slope_er[i, :])), s=2, color='b')
     ax.scatter(slope_er[i, low], z_grid[i] * np.ones(len(slope_er[i, low])), s=4, color='r')
     ax.scatter(np.nanmean(slope_er[i, low]), z_grid[i], s=20, color='r')
@@ -103,7 +107,7 @@ ax.set_xlim([1, 10**4])
 ax.set_ylim([-3000, 0])
 plot_pro(ax)
 if savee > 0:
-    f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_dg_per_shear_err_steep.png", dpi=200)
+    f.savefig('/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_dg_per_shear_err_' + str(tagg) + '.png', dpi=200)
 
 
 # all velocity anomalies colored by max abs vel attained
@@ -136,26 +140,33 @@ ax.plot(np.nanmean(this_anom, axis=1), z_grid, linewidth=2.2, color='k')
 plot_pro(ax)
 
 # RMS at different depths
-matplotlib.rcParams['figure.figsize'] = (6.5, 8)
-f, ax = plt.subplots()
-# ax.plot(np.nanmean(anoms, axis=1), z_grid, linewidth=2.2, color='k')
-mm = np.nanmean(anoms**2, axis=1)
+matplotlib.rcParams['figure.figsize'] = (9.5, 8)
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+anomy = v - mod_v_avg
+for i in range(np.shape(anomy)[1]):
+    ax1.plot(anomy[:, i], z_grid, color='k', linewidth=0.6)
+ax1.plot(np.nanmean(anomy, axis=1), z_grid, color='r')
+ax1.set_xlim([-.12, .12])
+ax1.set_xlabel('m/s')
+ax1.set_ylabel('Depth [m]')
+ax1.set_title(r'M/W Vel. Error ($u_{g}$ - $\overline{u_{model}}$) ('
+             + str(MOD['glide_slope']) + ':1, w=' + str(MOD['dg_w']) + ' m/s)')
+mm = np.nanmean(anomy**2, axis=1)
 mm_std = np.nan * np.ones(len(z_grid))
 for i in range(len(z_grid)):
-    mm_std[i] = np.nanstd(anoms[i, :]**2)
-# ax.errorbar(mm, z_grid, xerr=mm_std)
-ax.plot(mm, z_grid, linewidth=2.2, color='k')
-ax.plot(mm_std, z_grid, linewidth=1.5, color='k', linestyle='--')
+    mm_std[i] = np.nanstd(anomy[i, :]**2)
+ax1.text(0.04, -2600, str(np.shape(anomy)[1]) + ' profiles')
+ax2.plot(mm, z_grid, linewidth=2.2, color='k')
+ax2.plot(mm_std, z_grid, linewidth=1.5, color='k', linestyle='--')
 # ax.text(0.005, -2400, 'Mean Error = ' + str(np.round(np.nanmean(anoms), 3)) + ' m/s')
-ax.set_xlim([0, .01])
-ax.set_ylim([-3000, 0])
-ax.set_xlabel(r'm$^2$/s$^2$')
-ax.set_ylabel('Depth [m]')
-ax.set_title(r'Glider M/W Velocity Profile Error ($u_{g}$ - $\overline{u_{model}}$)$^2$ ('
-             + str(MOD['glide_slope']) + ':1, w=' + str(MOD['dg_w']) + ' m/s)')
-plot_pro(ax)
+ax2.set_xlim([0, .005])
+ax2.set_ylim([-3000, 0])
+ax2.set_xlabel(r'm$^2$/s$^2$')
+ax2.set_title(r'M/W Vel. RMS Error ($u_{g}$ - $\overline{u_{model}}$)$^2$')
+ax1.grid()
+plot_pro(ax2)
 if savee > 0:
-    f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_dg_vel2_error_steep.png", dpi=200)
+    f.savefig('/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_dg_vel_error_' + str(tagg) + '.png', dpi=200)
 
 # binss = np.arange(0, 0.002, 0.0001)
 # subax = f.add_axes([0.2, 0.75, .225, .08])
@@ -198,18 +209,18 @@ if savee > 0:
 # for spine in plt.gca().spines.values():
 #     spine.set_visible(False)
 
-f, ax = plt.subplots()
-for i in range(np.shape(anoms)[1]):
-    ax.plot(anoms[:, i], z_grid, color='#D3D3D3')
-ax.plot(np.nanmean(anoms, axis=1), z_grid, color='r', linewidth=2)
-ax.text(0.06, -2400, 'Mean Error = ' + str(np.round(np.nanmean(anoms), 3)) + ' m/s')
-ax.set_title(r'Glider M/W Velocity Profile Error ($u_{g}$ - $\overline{u_{model}}$)')
-ax.set_ylabel('z [m]')
-ax.set_xlabel('Velocity Error [m/s]')
-ax.set_xlim([-.15, .15])
-plot_pro(ax)
-if savee > 0:
-    f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_dg_vel_error_steep.png", dpi=200)
+# f, ax = plt.subplots()
+# for i in range(np.shape(anoms)[1]):
+#     ax.plot(anoms[:, i], z_grid, color='#D3D3D3')
+# ax.plot(np.nanmean(anoms, axis=1), z_grid, color='r', linewidth=2)
+# ax.text(0.06, -2400, 'Mean Error = ' + str(np.round(np.nanmean(anoms), 3)) + ' m/s')
+# ax.set_title(r'Glider M/W Velocity Profile Error ($u_{g}$ - $\overline{u_{model}}$)')
+# ax.set_ylabel('z [m]')
+# ax.set_xlabel('Velocity Error [m/s]')
+# ax.set_xlim([-.15, .15])
+# plot_pro(ax)
+# if savee > 0:
+#     f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_dg_vel_error_steep.png", dpi=200)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # --- PLOT ENERGY SPECTRA
@@ -264,7 +275,10 @@ ax2.scatter(sc_x, avg_KE_model[1:mm] / dk, color='k', s=20)
 ax2.plot([l_lim, sc_x[0]], avg_KE_model[0:2] / dk, color='k', linewidth=2)
 ax2.scatter(l_lim, avg_KE_model[0] / dk, color='k', s=25, facecolors='none')
 
-# test
+modeno = '1', '2', '3', '4', '5', '6', '7', '8'
+for j in range(len(modeno)):
+    ax2.text(sc_x[j], (avg_KE[j + 1] + (avg_KE[j + 1] / 2)) / dk, modeno[j], color='k', fontsize=10)
+
 limm = 5
 ax1.set_xlim([l_lim, 0.5 * 10 ** 2])
 ax2.set_xlim([l_lim, 0.5 * 10 ** 2])
@@ -288,4 +302,5 @@ handles, labels = ax1.get_legend_handles_labels()
 ax1.legend(handles, labels, fontsize=12)
 ax1.grid()
 plot_pro(ax2)
-# f.savefig("/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_04_18/model_dg_vel_energy_slow2.png", dpi=200)
+if savee > 0:
+    f.savefig('/Users/jake/Documents/baroclinic_modes/Meetings/meeting_19_05_17/model_dg_vel_energy_' + str(tagg) + '.png', dpi=200)